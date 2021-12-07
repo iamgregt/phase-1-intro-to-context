1c65e18a427dbe151449da0ac3f4fd4bfcbd0fa6
Gregory <taylor.gregory2@gmail.com>
2021-12-06 19:41:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+function createEmployeeRecord(array){
+    let obj = {
+     firstName : array[0],
+     familyName : array[1],
+     title : array[2],
+     payPerHour : array[3],
+     timeInEvents : [],
+     timeOutEvents : [],
+    }
+    return obj
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    let employeeRecords = arrayOfArrays.map(employee => createEmployeeRecord(employee))
+    return employeeRecords
+    
+}
+
+function createTimeInEvent(record, timeDate){
+    let splitTimeDate = timeDate.split(' ')
+    let dateSplit = splitTimeDate[0]
+    let timeSplit = splitTimeDate[1]
+    let timeInObj = {
+        type: "TimeIn",
+        date: dateSplit,
+        hour: parseInt(timeSplit)
+    }
+    record.timeInEvents.push(timeInObj)
+    return record
+}
+
+function createTimeOutEvent(record, timeDate){
+    let splitTimeDate = timeDate.split(' ')
+    let dateSplit = splitTimeDate[0]
+    let timeSplit = splitTimeDate[1]
+    let timeOutObj = {
+        type: "TimeOut",
+        date: dateSplit,
+        hour: parseInt(timeSplit)
+    }
+    record.timeOutEvents.push(timeOutObj)
+    return record
+}
+
+function hoursWorkedOnDate(record, date){
+    let timeIn = record.timeInEvents.find(e => {
+        return e.date === date
+    })
+    let timeOut = record.timeOutEvents.find(e => {
+        return e.date === date
+    })
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(record, date){
+    let pay = hoursWorkedOnDate(record, date) * record.payPerHour
+    return pay
+
+}
